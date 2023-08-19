4f7d15a5171bf73ba0ebb26a8631b0b6b4033a42
Pedro H <pedro_gonsalves@hotmail.com>
2023-08-19 01:02:45 -0300

    fix: remove print lines in insert function

--- src/main/scala/database/operations.scala
+++ src/main/scala/database/operations.scala
@@ -14,19 +14,15 @@ object operations {
   import PrivateExecutionContext._
   import slick.jdbc.PostgresProfile.api._
 
-  def insertPessoa(pessoa: Pessoa, numTries: Int = 3): Future[Unit] = {
+  def insertPessoa(pessoa: Pessoa): Future[Unit] = {
 
     val insertQuery = Tables.pessoaTable += pessoa
     val futureId: Future[Int] = connection.db.run(insertQuery)
     futureId.flatMap { _ =>
-      println(s"Query was successful.")
       Future.successful(())
     }.recoverWith {
-      case ex: Throwable if numTries > 1 =>
-        println(s"Query failed, reason: $ex")
-        insertPessoa(pessoa, numTries - 1)
       case ex: Throwable =>
-        println(s"Query failed after $numTries tries, reason: $ex")
+        println(ex)
         Future.failed(ex)
 
     }
