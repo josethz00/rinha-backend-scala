622be585ff68961f70db9de57dd832fea7072b56
Pedro H <pedro_gonsalves@hotmail.com>
2023-08-21 23:55:20 -0300

    rem: uuid generate by extension, now is generated in code and add function and triggers to work with search column

--- build_db.sql
+++ build_db.sql
@@ -1,9 +1,48 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+create extension if not exists "pg_trgm";
+
+
+CREATE OR REPLACE FUNCTION array_to_text(arr anyarray)
+    RETURNS text
+    IMMUTABLE
+    LANGUAGE SQL
+AS $$
+SELECT array_to_string(arr, '-');
+$$;
+
 
 create table if not exists pessoas (
-    id uuid DEFAULT uuid_generate_v4 (),
-    apelido varchar(32) not null,
-    nome varchar(100) not null,
-    nascimento date not null,
-    stack varchar(32)[]
+                                       id uuid NOT NULL,
+                                       apelido varchar(32) not null,
+                                       nome varchar(100) not null,
+                                       nascimento date not null,
+                                       stack varchar(32)[],
+                                       search VARCHAR(2000)
 );
+
+CREATE INDEX idx_search ON pessoas USING gin (search gin_trgm_ops);
+
+CREATE OR REPLACE FUNCTION generate_search_value(nome varchar, apelido varchar, stack varchar[])
+    RETURNS VARCHAR
+    IMMUTABLE
+    LANGUAGE SQL
+AS $$
+SELECT
+    LOWER(nome) || '-' || LOWER(apelido) ||
+    CASE
+        WHEN stack IS NULL THEN ''
+        ELSE '-' || array_to_text(stack, '-')
+        END
+$$;
+
+CREATE OR REPLACE FUNCTION update_search_column()
+    RETURNS TRIGGER AS $$
+BEGIN
+    NEW.search = generate_search_value(NEW.nome, NEW.apelido, NEW.stack);
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER update_search_on_insert
+    BEFORE INSERT ON pessoas
+    FOR EACH ROW
+EXECUTE FUNCTION update_search_column();
