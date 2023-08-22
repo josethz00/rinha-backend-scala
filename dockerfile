2ed35e9e60e3b5b4851e1635b78af98ccf011a37
Pedro H <pedro_gonsalves@hotmail.com>
2023-08-22 16:49:15 -0300

    fix: pessoas route, get by id and term were conflicting

--- dockerfile
+++
@@ -1,9 +0,0 @@
-FROM hseeberger/scala-sbt:8u302_1.5.5_2.13.6
-
-WORKDIR /app
-
-COPY . /app
-
-RUN sbt update
-
-CMD ["sbt", "run"]
--- src/main/scala/database/connection.scala
+++
@@ -1,7 +0,0 @@
-package database
-
-import slick.jdbc.PostgresProfile.api._
-
-object connection {
-  val db = Database.forConfig("postgres")
-}
