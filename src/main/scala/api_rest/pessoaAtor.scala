834a4c8a13c56eaf9a44f4c54033096887e29a9d
Pedro H <pedro_gonsalves@hotmail.com>
2023-08-21 23:48:18 -0300

    fix: changing getpessoaresponse actors. fix pessoa main class id type and finishing get and create pessoa actors

--- src/main/scala/api_rest/pessoaAtor.scala
+++ src/main/scala/api_rest/pessoaAtor.scala
@@ -8,8 +8,13 @@ import java.time.LocalDate
 import database.operations._
 
 import java.util.UUID
+import java.util.concurrent.Executors
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Failure, Success}
 
-final case class Pessoa(id:Option[UUID],
+
+final case class Pessoa(
+                        var id:Option[UUID] = None,
                         apelido: String,
                         nome: String,
                         nascimento: LocalDate,
@@ -18,26 +23,46 @@ final case class Pessoa(id:Option[UUID],
 
 object PessoaActor {
 
+  val executor = Executors.newFixedThreadPool(4)
+  implicit val ec: ExecutionContext = ExecutionContext.fromExecutorService(executor)
+
   sealed trait Command
 
-  final case class CreateUser(pessoa: Pessoa, replyTo: ActorRef[ActionPerformed]) extends Command
+  final case class CreatePessoa(pessoa: Pessoa, replyTo: ActorRef[PessoaCreated]) extends Command
 
-  final case class GetUser(nome: String, replyTo: ActorRef[GetUserResponse]) extends Command
+  final case class GetPessoa(uuidPessoa: String, replyTo: ActorRef[GetPessoaResponse]) extends Command
 
-  final case class GetUserResponse(maybeUser: Option[Pessoa])
+
+  final case class GetPessoaResponse(maybeUser: Option[Pessoa])
+
+  final case class PessoaCreated(id: String)
 
   final case class ActionPerformed(description: String)
 
+
   def apply(): Behavior[Command] = registry(Set.empty)
+
 
   private def registry(pessoas: Set[Pessoa]): Behavior[Command] =
     Behaviors.receiveMessage {
-      case CreateUser(pessoa, replyTo) =>
-        replyTo ! ActionPerformed(s"User ${pessoa.nome} created.")
+
+      case CreatePessoa(pessoa, replyTo) =>
+        val uuid = UUID.randomUUID()
+        pessoa.id = Some(uuid)
+        replyTo ! PessoaCreated(pessoa.id.get.toString)
         insertPessoa(pessoa)
         registry(pessoas + pessoa)
-      case GetUser(nomeFilter, replyTo) =>
-        replyTo ! GetUserResponse(pessoas.find(_.nome == nomeFilter))
+
+      case GetPessoa(uuidPessoa, replyTo) =>
+        val optionPerson = getPessoasByID(UUID.fromString(uuidPessoa))
+        optionPerson.onComplete {
+          case Success(pessoasEncontradas) =>
+            if (pessoasEncontradas.nonEmpty)
+            replyTo ! GetPessoaResponse(Some(pessoasEncontradas.head))
+            else {
+              replyTo ! GetPessoaResponse(None)
+            }
+        }
         Behaviors.same
     }
 }
